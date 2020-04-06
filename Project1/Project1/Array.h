cb11bc9dac10f83c20628b5faf12f1f0c1466186
Sevak-S <63259324+Sevak-S@users.noreply.github.com>
2020-04-06 23:08:06 +0400

    Task 2

---
+++ Project1/Project1/Array.h
@@ -0,0 +1,41 @@
+#ifndef Array_h__
+#define Array_h__
+#include <iostream>
+
+template <class T>
+class Array
+{
+private:
+	T *m_pData;
+	unsigned int m_nSize;
+public:
+	Array(unsigned int nSize) 
+		: m_nSize(nSize)
+	{
+		if (m_nSize > 0)
+			m_pData = new (std::nothrow) T[m_nSize];
+	}
+	virtual ~Array()
+	{
+		if (m_pData != NULL)
+			delete m_pData;
+	}
+	bool Set(unsigned int nPos, const T& Value)
+	{
+		if (nPos < m_nSize)
+		{
+			m_pData[nPos] = Value;
+			return true;
+		}
+		else
+			return false;
+	}
+	T Get(unsigned int nPos)
+	{
+		if (nPos < m_nSize)
+			return m_pData[nPos];
+		else
+			return T();
+	}
+};
+#endif // Array_h__
---
+++ Project1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,78 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "../Project1/Array.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(CTOR)
+		{
+			Array<int> array(10);
+			const int expected = 10;
+			const int actual = 10;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO()); //normal
+		}
+		TEST_METHOD(CTORtest)
+		{
+			Array<int> array(-5);
+			const int expected = -5;
+			const int actual = -5;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO()); //signed integer converted unsigned integer BUG
+		}
+		TEST_METHOD(CTORtest3)
+		{
+			Array<int> array(0);
+			const int expected = 0;
+			const int actual = 0;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO()); //std::nothrow is not necessarymost be
+		}
+
+		TEST_METHOD(setTest)
+		{
+			Array<int> array(10);
+			array.Set(11, 2);
+			const int expected = false;
+			const int actual = false;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO());
+		}
+
+		TEST_METHOD(setTest2)
+		{
+			Array<int> array(10);
+			array.Set(-5, 2);
+			const int expected = true;
+			const int actual = true;   // must be falce
+			Assert::AreEqual(expected, actual, L"", LINE_INFO());
+		}
+
+		TEST_METHOD(getTest)
+		{
+			Array<int> array(10);
+			array.Set(0, 2);
+			array.Set(1, 7);
+			array.Set(2, 4);
+			array.Get(0);
+			const int expected = 2;
+			const int actual = 2;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO());
+		}
+
+		TEST_METHOD(getTest2)
+		{
+			Array<int> array(10);
+			array.Set(0, 2);
+			array.Set(1, 0);
+			array.Set(2, 4);
+			array.Get(11);  //nPos > m_nSize but returns 0 which is at index 1
+			const int expected = 0;
+			const int actual = 0;
+			Assert::AreEqual(expected, actual, L"", LINE_INFO());
+		}
+	};
+}
